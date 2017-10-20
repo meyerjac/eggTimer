b587767141fd18ff81ef3caa09cef4b4dfe68072
jackson meyer <jackson.meyer7@gmail.com>
2017-10-20 11:49:07 -0700

    added Menu Bar Items and Tool Bar Items as well as instantiated a Timer Object

--- Menu Bars/ViewController.swift
+++ Menu Bars/ViewController.swift
@@ -9,10 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var timer = Timer()
 
+    @IBAction func cameraClicked(_ sender: Any) {
+        
+        print("camera was clicked")
+        
+        timer.invalidate()
+    }
+    
+    func processTimer() {
+        print("another second has passed")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.processTimer), userInfo: nil, repeats: true)
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
